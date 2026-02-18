5e3caf0aa815abb823fdd7ffe6fae495b6d2d5f0
SwayamWakodikar <swayam.w06@gmail.com>
2026-02-18 20:55:42 +0530

    feat: add packages and server, env.js for proper env handling

---
+++ server/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const app=express();
+const port=process.env.PORT;
+app.get('/',(req,res)=>{
+    res.send("Server Running")
+})
+app.listen(port,()=>{
+    console.log(`Server running at port ${port}`);
+})
