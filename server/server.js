bec13aaf2d613341e0d368d2c784bbac03c12f9f
SwayamWakodikar <swayam.w06@gmail.com>
2026-02-21 22:44:20 +0530

    feat: initialize Node.js Express server with basic configuration, database connection, API routes, and static file serving.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 import connectDB from "./config/db.js";
 import leadRoutes from "./routes/user.route.js";
 import errorHandler from "./middleware/error.middleware.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 connectDB();
@@ -14,6 +19,14 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/leads", leadRoutes);
+
+// Static files for frontend
+const clientPath = path.join(__dirname, "../client/dist");
+app.use(express.static(clientPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(clientPath, "index.html"));
+});
 
 app.use(errorHandler);
 
